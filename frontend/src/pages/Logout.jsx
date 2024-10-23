3222f073ed671ffdee55fbaedbdbf53585b19353
remolan1213 <r.lanugon@gmail.com>
2024-10-23 17:52:48 +0800

    style.css is modified

--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -11,7 +11,7 @@ const Logout = () => {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary" size="sm">Go somewhere</Button>
       </Card.Body>
     </Card>
   );
